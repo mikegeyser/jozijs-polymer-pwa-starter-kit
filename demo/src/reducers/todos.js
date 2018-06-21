fc7b288ed14e920efefb436f4f2be5ff6e0a7155
Mike Geyser <mikegeyser@gmail.com>
2018-06-21 06:41:19 +0200

    More WIP

--- demo/src/reducers/todos.js
+++ demo/src/reducers/todos.js
@@ -1,13 +1,20 @@
 class Todo {
-    constructor(title) {
+    constructor(title, completed = false) {
         this.id = Number(new Date());
         this.title = title;
         this.editing = false;
-        this.completed = false;
+        this.completed = completed;
     }
 }
 
-const defaultState = [new Todo("This is a test.")];
+const defaultState = [
+    new Todo("Add a todo", true),
+    new Todo("List todos", true),
+    new Todo("View a todo", true),
+    new Todo("Complete a todo", true),
+    new Todo("Delete a todo"),
+    new Todo("Edit a todo"),
+];
 
 export const todos = (state = defaultState, action) => {
     switch (action.type) {
